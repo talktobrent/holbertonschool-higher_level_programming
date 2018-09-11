6e30c443867b877885773ea7ea2ab2732ec60f3a
talktobrent <talktobrent@gmail.com>
2018-09-11 15:02:16 -0700

    Fixed style error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for a in range(0, 100):
     if a < 99:
-        print("{0:02}, ".format(a), end = "")
+        print("{0:02}, ".format(a), end="")
     else:
         print("{0:02}".format(a))
