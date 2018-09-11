f6a38fb01414922aab02ac5a465b8f0c3955b6f9
talktobrent <talktobrent@gmail.com>
2018-09-11 02:12:55 +0000

    Created function

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+  * check_cycle - checks if linked list has a cycle
+  * @list: head of lined list given
+  * Return: 1 if cycle, 0 if not
+  */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list;
+	listint_t *slow = list;
+
+	while (fast->next != NULL && list != NULL)
+	{
+		fast = fast->next;
+		if (fast->next != NULL)
+			fast = fast->next;
+		if (fast == slow)
+			return (1);
+		slow = slow->next;
+	}
+
+	return (0);
+}
