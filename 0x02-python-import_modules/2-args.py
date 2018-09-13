a956794cc21464c2d06c3aa44ecf988477e49e2f
talktobrent <talktobrent@gmail.com>
2018-09-13 15:00:44 -0700

    Created function

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    print("{:d} argument".format(len(argv) - 1), end="")
+    if len(argv) - 1 > 1:
+        print("s")
+    else:
+        print()
+    for count in range(1, len(argv)):
+        print("{:d}: {:s}".format(count, argv[count]))
