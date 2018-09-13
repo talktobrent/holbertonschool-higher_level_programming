fff0a84cf359c4a6b9c8bfdd871ba1decf5cd81e
talktobrent <talktobrent@gmail.com>
2018-09-13 15:17:35 -0700

    Changed print style for 0 arguments passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) > 1:
-        print("{:d} argument".format(len(argv) - 1), end="")
-        if len(argv) - 1 > 1:
-            print("s:")
-        else:
-            print(":")
-        for count in range(1, len(argv)):
-            print("{:d}: {:s}".format(count, argv[count]))
+    from sys import argv, exit
+    print("{:d} argument".format(len(argv) - 1), end="")
+    if len(argv) == 1:
+        print("s.")
+        exit()
+    elif len(argv) - 1 > 1:
+        print("s:")
     else:
-        print(".")
+        print(":")
+    for count in range(1, len(argv)):
+        print("{:d}: {:s}".format(count, argv[count]))
