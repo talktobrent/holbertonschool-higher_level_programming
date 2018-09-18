cc7da24436b55df4c73d0c53daecfcfb2d0e58f9
talktobrent <talktobrent@gmail.com>
2018-09-18 14:30:04 -0700

    Changed method to using 'range' to position 'x' at end of list, and increment lower. Added condition for empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    copylist = my_list[:]
-    for x in reversed(copylist):
-        print("{:d}".format(x))
+    if len(my_list) == 0:
+        return None
+    for x in range((len(my_list) - 1), -1, -1):
+        print("{:d}".format(my_list[x]))
