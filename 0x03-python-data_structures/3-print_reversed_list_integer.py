a57bc660d6e4ced460a04829305fe58f4427ed39
talktobrent <talktobrent@gmail.com>
2018-09-18 14:34:22 -0700

    Changed list 'len' test to simply 'if not'

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list:
         return None
     for x in range((len(my_list) - 1), -1, -1):
         print("{:d}".format(my_list[x]))
