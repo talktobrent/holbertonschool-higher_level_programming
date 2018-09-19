10535f9bbe2db0e04e6c1c339261e9bb8b6154f5
talktobrent <talktobrent@gmail.com>
2018-09-18 17:17:48 -0700

    Added '>' to both conditions in first 'if' statement, rather than combined

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) and len(tuple_b) > 1:
+    if len(tuple_a) > 1 and len(tuple_b) > 1:
         newtup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
         return newtup
     else:
