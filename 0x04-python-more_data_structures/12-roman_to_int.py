f5fd445dd99794d32580d345450d4115eef13cef
talktobrent <talktobrent@gmail.com>
2018-09-20 17:42:44 -0700

    Added conditions to accomodate 4 and 9 value roman numerals

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,13 @@ def roman_to_int(roman_string):
         rdic = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                 'C': 100, 'D': 500, 'M': 1000}
         number = 0
-        for char in roman_string:
+        for index, char in enumerate(roman_string):
             for r, n in rdic.items():
                 if char == r:
+                    if char == 'I' and index != len(roman_string) - 1:
+                        if roman_string[index + 1] in ('X', 'V'):
+                            number -= 2
+                        else:
+                            pass
                     number += n
     return (number)
