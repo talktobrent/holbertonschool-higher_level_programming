55c840e62dd0bf7d51065759a113e31a2a51b245
talktobrent <talktobrent@gmail.com>
2018-10-02 20:44:02 -0700

    Changed tuple length check to look for exactly 2 versus less than 3

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
     def position(self, value):
         if type(value) is not tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif len(value) > 2:
+        elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
