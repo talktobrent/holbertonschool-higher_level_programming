8749542105cf1a252ab9a47d0787e29f878b8962
talktobrent <talktobrent@gmail.com>
2018-10-02 18:59:35 -0700

    Created class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+class Square:
+    """  a square
+
+    Attributes:
+        __size (int): Size of square
+        area (self): area of sqaure
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
