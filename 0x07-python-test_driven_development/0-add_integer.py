9f7d005f4366136d38be29b7ff8c9dd3bd745993
talktobrent <talktobrent@gmail.com>
2018-10-06 13:13:55 -0700

    Removed case for None

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
         TypeError: a must be an integer
         TypeError: b must be an integer
     """
-    if type(a) not in [int, float] or a is None:
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
