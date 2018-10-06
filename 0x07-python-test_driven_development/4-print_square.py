1501161e6a9ba5663cb7ae5a382ef3994fa367b0
talktobrent <talktobrent@gmail.com>
2018-10-06 03:30:59 -0700

    Added case for negative float to catch proper error

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -16,10 +16,11 @@ def print_square(size):
         ValueError: size must be >= 0
 
     """
+    if type(size) in [int, float] and size < 0:
+        raise ValueError("size must be >= 0")
+
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
 
     for x in range(size):
         print("#" * size)
