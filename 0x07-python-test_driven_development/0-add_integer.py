63b87511cbd475656e1c30cede9b9eb66cc0c3b5
talktobrent <talktobrent@gmail.com>
2018-10-06 04:03:32 -0700

    Added test condition for 'None' passed to function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,7 +17,7 @@ def add_integer(a, b=98):
         TypeError: a must be integer
         TypeError: b must be integer
     """
-    if type(a) not in [int, float]:
+    if type(a) not in [int, float] or a is None:
         raise TypeError("a must be integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be integer")
