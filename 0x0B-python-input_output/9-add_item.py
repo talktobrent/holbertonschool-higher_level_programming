5f247475d80ef27f219c1f96e9922a90109b84bc
talktobrent <talktobrent@gmail.com>
2018-10-16 17:04:08 -0700

    Created function

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+
+try:
+    ob = load_from_json_file("add_item.json")
+    ob.extend(argv[1:])
+    save_to_json_file(ob, "add_item.json")
+except:
+    save_to_json_file(argv[1:], "add_item.json")
