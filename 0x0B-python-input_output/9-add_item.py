71ad35add8cadee94c2000c26e734db19383ef96
talktobrent <talktobrent@gmail.com>
2018-10-16 17:11:12 -0700

    Added docstring

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" adds arguments to a JSON file """
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
