b255f711526154259f23f0e023282a50a9ec2177
talktobrent <talktobrent@gmail.com>
2018-10-16 20:04:35 -0700

    Created function

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Student module
+"""
+
+
+class Student:
+    """ A student
+
+    Atrributes:
+        first_name (str)
+        last_name (str)
+        age (int)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ initializes a Student
+
+        Args:
+            first_name (str)
+            last_name (str)
+            age (int)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ JSON representation
+
+        Args:
+            attrs (list): list of strings
+
+        Returns:
+            Dictionary
+        """
+        if type(attrs) == list:
+            for x in attrs:
+                if type(x) is not str:
+                    return self.__dict__
+            return {key: self.__dict__[key] for key in attrs
+                    if key in self.__dict__}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces public attributes
+
+        Args:
+            json (dict): new attributes
+        """
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
