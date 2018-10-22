223843608596008d4c4f7213d3a89c0f1e096d49
talktobrent <talktobrent@gmail.com>
2018-10-22 12:38:32 -0700

    added tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,6 +3,8 @@
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -12,10 +14,10 @@ class TestBase(unittest.TestCase):
         """ checks id creation
         """
         b1 = Base()
-        self.assertEqual(b1.id, 1)
+        self.assertEqual(b1.id, Base._Base__nb_objects)
 
         b2 = Base()
-        self.assertEqual(b2.id, 2)
+        self.assertGreater(b2.id, b1.id)
 
         b4 = Base(12)
         self.assertEqual(b4.id, 12)
@@ -24,11 +26,67 @@ class TestBase(unittest.TestCase):
         self.assertEqual(b6.id, 12)
 
         b5 = Base()
-        self.assertEqual(b5.id, 3)
+        self.assertEqual(b5.id, Base._Base__nb_objects)
 
-        del(b5)
-        b5 = Base()
-        self.assertEqual(b5.id, 4)
+        x = Base._Base__nb_objects + 1
+
+        bno = Base(0)
+        self.assertNotEqual(bno.id, x)
+
+        b6 = Base()
+        self.assertEqual(b6.id, x)
 
         b7 = Base(None)
-        self.assertEqual(b7.id, 5)
+        self.assertEqual(b7.id, x + 1)
+
+    def test_jsonstring(self):
+        """ test json to string function
+        """
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertIsInstance(json_dictionary, str)
+        self.assertEqual(json_dictionary[:3], "[{\"")
+
+        json_dictionary = Base.to_json_string([dictionary, {}])
+        json_dictionary = Base.to_json_string([{}])
+        self.assertEqual(json_dictionary, "[{}]")
+
+        with self.assertRaises(TypeError):
+            r1 = Rectangle(10, 7, 2, 8)
+            dictionary = r1.to_dictionary()
+            json_dictionary = Base.to_json_string([dictionary, r1])
+
+    def test_json_to_file(self):
+        """ test json to file
+        """
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open("Rectangle.json", "r") as f:
+            string = f.read()
+            self.assertEqual(string[:3], "[{\"")
+
+    def test_from_json_string(self):
+        """ tests string to dict
+        """
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+            ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertCountEqual(list_output, list_input)
+
+    def test_dict_to_instance(self):
+        """ tests create method
+        """
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertEqual(str(r1), str(r2))
+
+        with self.assertRaises(AttributeError):
+
+            Square.create(**r1_dictionary)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -5,6 +5,7 @@
 import unittest
 from models.square import Square
 
+
 class TestSquare(unittest.TestCase):
     """ Tests Square class
     """
@@ -17,6 +18,10 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(a.x, 2)
         self.assertEqual(a.y, 3)
         self.assertEqual(a.id, 4)
+
+        a = Square(7)
+        self.assertEqual(a.size, 7)
+
         a.size = 9
         self.assertEqual(a.size, 9)
         self.assertEqual(a._Rectangle__width, 9)
@@ -25,16 +30,46 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(a._Rectangle__width, 1)
         self.assertEqual(a._Rectangle__height, 1)
 
-        a = Square(1, 1, 1, 1)
         with self.assertRaises(AttributeError):
             print(a.width)
             print(a.height)
             a.height = 5
             a.width = 5
+
+        with self.assertRaises(TypeError):
+            a = Square(1, "2", 1, 1)
+
+    def test_update(self):
+        """ update test
+        """
+        args = ()
+        kwargs = {}
+        a = Square(1, 2, 3, 4)
+        a.update(*args, **kwargs)
+        self.assertEqual(a.size, 1)
+        self.assertEqual(a.x, 2)
+        self.assertEqual(a.y, 3)
+        self.assertEqual(a.id, 4)
+        a.update(0)
+        self.assertEqual(a.id, 0)
+
+        a = Square(1, 1, 1, 1)
+        with self.assertRaises(AttributeError):
             a.update(width=5)
             a.update(height=5)
             a.update(size=5, width=5)
             a.update(size=4, height=5)
             a.update(dog=4)
             a.update(width=None)
-            a.update(heigth=None)
+            a.update(height=None)
+
+        a = Square(1, 1, 1, 1)
+        with self.assertRaises(ValueError):
+            a.update(size=0)
+
+    def test_dict(self):
+        """ dict test
+        """
+        s1 = Square(10, 2, 1, 1)
+        d = s1.to_dictionary()
+        self.assertEqual(d, {'id': 1, 'x': 2, 'size': 10, 'y': 1})
