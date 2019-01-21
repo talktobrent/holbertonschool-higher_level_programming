4ee1a4654621660107076e731f232a7866095775
talktobrent <talktobrent@gmail.com>
2019-01-21 15:57:59 -0800

    Created script

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" fetch a website header """
+
+from urllib import parse, request
+from sys import argv
+
+if __name__ == "__main__":
+
+    data = parse.urlencode({'email': argv[2]}).encode()
+
+    with request.urlopen(argv[1], data) as page:
+        print(page.read().decode('utf-8'))
