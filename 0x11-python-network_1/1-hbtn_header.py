3b955ff554599a9a051b70a1ce11669f9737d318
talktobrent <talktobrent@gmail.com>
2019-01-21 15:19:12 -0800

    Added import guard

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,5 +4,6 @@
 import urllib.request
 from sys import argv
 
-with urllib.request.urlopen(argv[1]) as page:
-    print(page.info().get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as page:
+        print(page.info().get('X-Request-Id'))
