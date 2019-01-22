62af6002227a02d23df43725adda78a1b5636486
talktobrent <talktobrent@gmail.com>
2019-01-21 18:39:42 -0800

    Created script

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" fetch a website and print status code if an error, else print text """
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) < 2:
+        val = ""
+    else:
+        val = argv[1]
+
+    page = requests.post('http://0.0.0.0:5000/search_user', {'q': val})
+    try:
+        print("[{}] {}".format(page.json().pop('id'), page.json().pop('name')))
+    except ValueError:
+        print("Not a valid JSON")
+    except KeyError:
+        print("No result")
