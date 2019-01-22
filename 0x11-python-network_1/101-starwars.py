69546e00b19df5f4a2a3df77fa1e6c01a1e411f4
talktobrent <talktobrent@gmail.com>
2019-01-21 20:55:11 -0800

    Created script

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" get starwars characters """
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    page = requests.get('https://swapi.co/api/people/?search={}'.
+                        format(argv[1]))
+    names = []
+    try:
+        while True:
+            names.extend(page.json().pop('results'))
+            if page.json().get('next'):
+                page = requests.get(page.json().pop('next'))
+            else:
+                break
+        print("Number of results: {}".format(len(names)))
+        for char in names:
+            print(char.pop('name'))
+    except ValueError:
+        print("Not a valid JSON")
+    except KeyError:
+        print("No result")
