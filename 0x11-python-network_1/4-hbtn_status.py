09de1e3165e55cfd51bad282e20fc9eb277b75ba
talktobrent <talktobrent@gmail.com>
2019-01-21 16:44:32 -0800

    Changed script to only use requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ fetch a website """
 
-import urllib.request
+import requests
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as page:
-    output = page.read()
-print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".
-      format(type(output), output))
+page = requests.get('https://intranet.hbtn.io/status')
+print("Body response:\n\t- type: {}\n\t- content: {}".format(type(page.text), page.text))
