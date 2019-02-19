755be7bbf9f7ff232ca88379d4b12fa950f5450e
talktobrent <talktobrent@gmail.com>
2019-02-18 18:51:51 -0800

    Simplified logic

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -5,17 +5,10 @@ const dict = require('./101-data.js').dict;
 let newdict = {};
 
 for (let x in dict) {
-  if (dict[x].toString() in newdict) {
-    continue;
-  }
-  for (let y in dict) {
-    if (dict[y] === dict[x]) {
-      if (newdict[dict[x].toString()]) {
-        newdict[dict[x].toString()].push(y);
-      } else {
-        newdict[dict[x].toString()] = [y];
-      }
-    }
+  if (newdict[dict[x].toString()]) {
+    newdict[dict[x].toString()].push(x);
+  } else {
+    newdict[dict[x].toString()] = [x];
   }
 }
 console.log(newdict);
