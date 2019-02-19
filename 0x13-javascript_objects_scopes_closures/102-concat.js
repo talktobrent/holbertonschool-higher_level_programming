18e0c148e365b2df1ba0da85542ecd9029aa3e33
talktobrent <talktobrent@gmail.com>
2019-02-18 21:13:15 -0800

    Changed to write to file, rather than print

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -4,5 +4,4 @@ let fs = require('fs');
 
 let buffer = fs.readFileSync(process.argv[2], { encoding: 'utf-8', flag: 'r' });
 let buffer2 = fs.readFileSync(process.argv[3], { encoding: 'utf-8', flag: 'r' });
-
-console.log(buffer.concat(buffer2).trim());
+fs.writeFileSync(process.argv[4], buffer.concat(buffer2));
