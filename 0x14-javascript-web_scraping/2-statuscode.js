031b707613a7142dee267068f686578438eea8c4
talktobrent <talktobrent@gmail.com>
2019-02-19 16:11:44 -0800

    Added 'on' method to handle only response object

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 
 const request = require('request');
 
-request(process.argv[2], function (error, response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log('code: ' + response.statusCode);
 });
