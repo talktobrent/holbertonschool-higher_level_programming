0e94541ba422af623d29fd101091a71f9842ed36
talktobrent <talktobrent@gmail.com>
2019-02-19 17:12:05 -0800

    Added error printing

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,5 @@ const request = require('request');
 
 let endpoint = 'http://swapi.co/api/films/' + process.argv[2];
 request(endpoint, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
-  }
+  console.log(error || JSON.parse(body).title);
 });
